4febf073612c224014fcc6af92ff8666d6f18587
shota <sunashoo@gmail.com>
2013-09-06 01:51:01 +0900

    コミット

---
+++ src/com/mycompany/myapp/sample.java
@@ -0,0 +1,40 @@
+package com.mycompany.myapp;
+
+//<=�R�����g(��s�_���j
+/*
+ ** �R�����g(��s�ł���)
+ * 
+ */
+
+ class Ex1_2_1 {
+
+	/**
+	 * �h�L�����[�V�����R�����g
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//��s���ĕ\������
+		System.out.println("minus!");
+		System.out.println("zero!");
+		System.out.println("plus!");
+
+		System.out.print("Hello");
+		System.out.print("����ɂ���");
+		//�\�����邾���ŉ�s���Ȃ�
+		System.out.println("");
+
+		System.out.println("123+10��?");
+		System.out.println("----------");
+		System.out.println("133�ł�");
+		System.out.println("����ł�!");
+	   
+	    }
+		
+}
+
+
+   	
+
+
+
+
